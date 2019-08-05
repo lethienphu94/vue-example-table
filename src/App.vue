64042fd2a951c709eaf07b57c8ad56e4b36dfb8e
lethienphu94 <phuk54cntt@gmail.com>
2019-08-05 20:43:15 +0700

    test boostrap vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <button type="button" class="btn btn-primary">TEST</button>
+    <div>
+      <b-alert show variant="primary">Primary Alert</b-alert>
+      <b-alert show variant="secondary">Secondary Alert</b-alert>
+      <b-alert show variant="success">Success Alert</b-alert>
+      <b-alert show variant="danger">Danger Alert</b-alert>
+      <b-alert show variant="warning">Warning Alert</b-alert>
+      <b-alert show variant="info">Info Alert</b-alert>
+      <b-alert show variant="light">Light Alert</b-alert>
+      <b-alert show variant="dark">Dark Alert</b-alert>
+      <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div>
+      <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+        <b-button-group class="mx-1">
+          <b-button>&laquo;</b-button>
+          <b-button>&lsaquo;</b-button>
+        </b-button-group>
+        <b-button-group class="mx-1">
+          <b-button>Edit</b-button>
+          <b-button>Undo</b-button>
+          <b-button>Redo</b-button>
+        </b-button-group>
+        <b-button-group class="mx-1">
+          <b-button>&rsaquo;</b-button>
+          <b-button>&raquo;</b-button>
+        </b-button-group>
+      </b-button-toolbar>
+    </div>
   </div>
 </template>
 
